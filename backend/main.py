101d0f4be214628afb36b6fe92252f54fc1a74cd
Temake <teminioluwaopemipo@gmail.com>
2025-09-09 08:00:41 +0100

    Fix: Account model

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from models.database import create_db_and_tables
-
+from models.account import *
+from models.database import *
+from models.projects import *
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
-app = FastAPI(lifespan=lifespan, title="Student Data Center")
+app = FastAPI(lifespan=lifespan, title="Scholar Base")
 
 @app.get("/")
 def root():
